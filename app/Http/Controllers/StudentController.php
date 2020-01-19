0d84038682e14837aef0232f3a45d30579d9961a
Osman Forhad <osmanforhad93@gmail.com>
2020-01-19 13:15:11 +0600

    modifi StudentController index method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -14,7 +14,8 @@ class StudentController extends Controller
      */
     public function index()
     {
-        //
+        $students = Student::all();
+        return view('student.index');
     }
 
     /**
