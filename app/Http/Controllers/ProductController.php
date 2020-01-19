3493646ab00ec8bffa286e96beab9009ba40b795
Osman Forhad <osmanforhad93@gmail.com>
2020-01-19 12:10:24 +0600

    modifi ProductController as i need

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use PDF;
 
 class ProductController extends Controller
 {
-    //
+    public function htmltopdfview(Request $request)
+    {
+        $products = Products::all();
+
+        view()->share('products', $products);
+
+        if ($request->has('download')) {
+            $pdf = PDF::loadview('htmltopdfview');
+            return $pdf->download('htmltopdfview');
+        } //end of the if condition
+
+        return view('htmltopdfview');
+    } //end of the htmltopdfview method
 }
